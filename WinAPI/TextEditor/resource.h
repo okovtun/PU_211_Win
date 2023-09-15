988b0538fa5b9c57e4dafed46dcafd0c4d27cae7
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-15 13:18:21 +0300

    TextEditor day 1

---
+++ WinAPI/TextEditor/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDC_EDIT						100
+#define IDR_MAIN_MENU                   101
+#define ID_HELP_ABOUT                   40001
+#define ID_EDIT_FONT                    40002
+#define ID_FILE_NEW                     40003
+#define ID_FILE_OPEN                    40004
+#define ID_FILE_SAVE                    40005
+#define ID_FILE_SAVEAS                  40006
+#define ID_FILE_EXIT                    40007
+#define ID_EDIT_COPY                    40008
+#define ID_EDIT_PAST                    40009
+#define ID_EDIT_FIND                    40010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
